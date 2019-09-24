ef981dbfc190d91405114ca5694dfe25de6db5b1
Luciano <lucianorufo97@gmail.com>
2019-09-24 17:01:52 -0300

    Deleted Id from lists

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -4,7 +4,6 @@ import { List, Datagrid, TextField, NumberField } from 'react-admin';
 export const ItemList = props => (
     <List {...props}>
         <Datagrid rowClick="edit">
-            <TextField source="id" />
             <TextField source="bar_code" />
             <TextField source="name" />
             <NumberField source="price" />
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -5,7 +5,6 @@ import { List, Datagrid, TextField} from 'react-admin';
 export const UserList = props => (
     <List {...props}>
         <Datagrid rowClick="edit">
-            <TextField source="id" />
             <TextField source="name" />
         </Datagrid>
     </List>
