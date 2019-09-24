ac7c7b70b596d9ed4a5e254bb1f96a83049acdf9
Luciano <lucianorufo97@gmail.com>
2019-09-24 16:00:41 -0300

    Item create error fix

--- src/components/ItemCreate.js
+++ src/components/ItemCreate.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Create, SimpleForm, TextInput, NumberInput, DateInput } from 'react-admin';
 
-export const PurchaseCreate = (props) => (
+export const ItemCreate = (props) => (
     <Create {...props}>
         <SimpleForm>
             <TextInput source="Item Name" />
