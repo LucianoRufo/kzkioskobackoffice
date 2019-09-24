cbdbc369218f287da6e1944e728282479e1d4918
Ignacio Picun <ignaciopicun@protonmail.com>
2019-09-24 16:10:59 -0300

    Added environment variables

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import './App.css';
-import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser, UserMenu, AppBar, Layout } from 'react-admin';
-import { UserList } from './components/UserList';
-import { ItemList } from './components/ItemList';
-import { PurchaseList } from './components/PurchaseList'
-import { PurchaseCreate } from './components/PurchaseCreate'
-import { PurchaseEdit } from './components/PurchaseEdit'
+import React from "react";
+import "./App.css";
+import { Admin, Resource, UserMenu, AppBar, Layout } from "react-admin";
+import { PurchaseList } from "./components/PurchaseList";
+import { PurchaseCreate } from "./components/PurchaseCreate";
+import { PurchaseEdit } from "./components/PurchaseEdit";
+import { ItemEdit } from "./components/ItemEdit";
+import { ItemList } from "./components/ItemList";
 
-import { withStyles } from '@material-ui/core/styles';
-import Avatar from '@material-ui/core/Avatar';
+import { withStyles } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
 
 import {
   FirebaseAuthProvider,
@@ -17,13 +17,13 @@ import {
 } from "react-admin-firebase";
 
 const config = {
-  apiKey: "AIzaSyCzVGkcoS8pGcftcYYSUr-gMHKrDzQ_zek",
-  authDomain: "elkioskitodekaizen.firebaseapp.com",
-  databaseURL: "https://elkioskitodekaizen.firebaseio.com",
-  projectId: "elkioskitodekaizen",
-  storageBucket: "elkioskitodekaizen.appspot.com",
-  messagingSenderId: "651296690538",
-  appId: "1:651296690538:web:9982ba6c9c6b7a17a79513"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID
 };
 
 const options = {};
@@ -35,23 +35,17 @@ const firebaseRealtime = FirebaseRealTimeSaga(dataProvider);
 //Bar with profile
 const myCustomIconStyle = {
   avatar: {
-      height: 30,
-      width: 30,
-  },
+    height: 30,
+    width: 30
+  }
 };
-const MyCustomIcon = withStyles(myCustomIconStyle)(
-  ({ classes }) => (
-      <Avatar
-          className={classes.avatar}
-          src=""
-      />
-  )
-);
+const MyCustomIcon = withStyles(myCustomIconStyle)(({ classes }) => (
+  <Avatar className={classes.avatar} src="" />
+));
 
-const MyUserMenu = props => (<UserMenu {...props} icon={<MyCustomIcon />} />);
+const MyUserMenu = props => <UserMenu {...props} icon={<MyCustomIcon />} />;
 const MyAppBar = props => <AppBar {...props} userMenu={<MyUserMenu />} />;
 const MyLayout = props => <Layout {...props} appBar={MyAppBar} />;
-
 
 //<Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
 function App() {
@@ -61,7 +55,12 @@ function App() {
       authProvider={authProvider}
       customSagas={[firebaseRealtime]}
     >
-      <Resource name="purchases" list={PurchaseList} edit={PurchaseEdit} create={PurchaseCreate} />
+      <Resource
+        name="purchases"
+        list={PurchaseList}
+        edit={PurchaseEdit}
+        create={PurchaseCreate}
+      />
       <Resource name="items" list={ItemList} edit={ItemEdit} />
     </Admin>
   );
